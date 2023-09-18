5659a0949940659eb6def4558c7390e5e737f707
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-18 11:55:34 -0600

    Initial Commit

---
+++ PracticeDubDubGrub/PracticeDubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeDubDubGrubApp.swift
+//  PracticeDubDubGrub
+//
+//  Created by Mitch Andrade on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeDubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
