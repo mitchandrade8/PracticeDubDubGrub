6db433c870e8de2e38b7082bcc88729cfd6fc582
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-19 15:27:55 -0600

    -- add a region to MapView and a asset logo to the top of the map

--- PracticeDubDubGrub/LocationMapView.swift
+++ PracticeDubDubGrub/LocationMapView.swift
@@ -6,11 +6,29 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                    longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.1,
+                                                                          longitudeDelta: 0.1))
+    
     var body: some View {
         ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
             
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .shadow(radius: 12)
+                
+                Spacer()
+            }
         }
     }
 }
