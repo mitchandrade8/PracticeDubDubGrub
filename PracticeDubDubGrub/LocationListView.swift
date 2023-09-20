5bbe7ce4761c48333cd99b526c5e6868232174d9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-19 23:05:36 -0600

    -- Current stop point: Building and refactoring the grub spot locations list, extracting the image in a refactor

--- PracticeDubDubGrub/LocationListView.swift
+++ PracticeDubDubGrub/LocationListView.swift
@@ -18,9 +18,34 @@ struct LocationListView: View {
                             .scaledToFit()
                             .frame(height: 80)
                             .clipShape(Circle())
+                            .padding(.vertical, 6)
                         
-                        Text("Location")
-                            .font(.system(size: 32, weight: .regular, design: .serif))
+                        VStack(alignment: .leading) {
+                            Text("Location")
+                                .font(.system(size: 32, weight: .thin, design: .serif))
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            
+                            HStack {
+                                Image("default-avatar")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 35)
+                                    .clipShape(Circle())
+                                
+                                Image("default-avatar")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 35)
+                                    .clipShape(Circle())
+                                
+                                Image("default-avatar")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 35)
+                                    .clipShape(Circle())
+                            }
+                        }
                     }
                 }
             }
