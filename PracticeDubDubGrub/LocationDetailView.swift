95f78df65c96c02f0778dbbfd71596d1808e6469
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-20 06:54:25 -0600

    -- Close but just small tweaks and simplier ways to accomplish the same thing, great review though.

--- PracticeDubDubGrub/LocationDetailView.swift
+++ PracticeDubDubGrub/LocationDetailView.swift
@@ -9,75 +9,84 @@ import SwiftUI
 
 struct LocationDetailView: View {
     var body: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(.bottom)
-                
-                HStack {
-                    Image(systemName: "mappin.and.ellipse")
+        NavigationView {
+            VStack {
+                VStack() {
+                    Image("default-banner-asset")
                         .resizable()
-                        .scaledToFit()
-                        .frame(height: 15)
-                        .foregroundColor(.secondary)
+                        .scaledToFill()
+                        .frame(height: 140)
+                        
+                    HStack() {
+                        Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                        
+                        Spacer()
+//                        Image(systemName: "mappin.and.ellipse")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(height: 15)
+//                            .foregroundColor(.secondary)
+//                        
+//                        Text("15 Market St Ste 40")
+//                            .font(.system(size: 12, weight: .thin, design: .serif))
+//                            .foregroundColor(.secondary)
+                        
+                    }
                     
-                    Text("15 Market St Ste 40")
-                        .font(.system(size: 12, weight: .thin, design: .serif))
-                        .foregroundColor(.secondary)
-                    
-                }
-                .padding(.leading)
-                
-            }
-            .padding(.bottom, 40)
-            
-            Text("It is Chipotle. Enough said.")
-            
-            HStack(spacing: 20) {
-                Button(action: {}, label: {
-                    Image(systemName: "location.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 60)
-                        .foregroundColor(.brandPrimary)
-                })
-                
-                Button(action: {}, label: {
-                    Image(systemName: "network")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 60)
-                        .foregroundColor(.brandPrimary)
-                })
-                
-                Button(action: {}, label: {
-                    Image(systemName: "phone.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 60)
-                        .foregroundColor(.brandPrimary)
-                })
-                
-                Button(action: {}, label: {
-                    Image(systemName: "person.crop.circle.fill.badge.xmark")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 60)
-                        .foregroundColor(.brandPrimary)
-                })
-            }
-            
-            Spacer()
-            
-            Section {
-                List {
                     
                 }
-            } header: {
-                Text("Who is here???")
+                .padding(.bottom, 40)
+                
+                Text("It is Chipotle. Enough said.")
+                
+                HStack(spacing: 20) {
+                    Button(action: {}, label: {
+                        Image(systemName: "location.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 60)
+                            .foregroundColor(.brandPrimary)
+                    })
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "network")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 60)
+                            .foregroundColor(.brandPrimary)
+                    })
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "phone.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 60)
+                            .foregroundColor(.brandPrimary)
+                    })
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "person.crop.circle.fill.badge.xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 60)
+                            .foregroundColor(.brandPrimary)
+                    })
+                }
+                
+                Spacer()
+                
+                Section {
+                    List {
+                        
+                    }
+                } header: {
+                    Text("Who is here???")
+                }
             }
+            .navigationTitle("Location Name")
+            .navigationBarTitleDisplayMode(.inline)
         }
             
     }
