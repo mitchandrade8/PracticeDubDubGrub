f4e395b9b106b9c36e8939744666c3b6a5a33f5a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-20 06:16:22 -0600

    -- Accepted challenge to build out UI of detail view

---
+++ PracticeDubDubGrub/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  PracticeDubDubGrub
+//
+//  Created by Mitch Andrade on 9/20/23.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
