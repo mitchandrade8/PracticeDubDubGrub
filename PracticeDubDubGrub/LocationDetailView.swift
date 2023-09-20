8a1d1c0e58f8172283333c15dad679cee85429d7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-20 06:17:01 -0600

    -- Start of LocationDetailView UI Challenge no help

--- PracticeDubDubGrub/LocationDetailView.swift
+++ PracticeDubDubGrub/LocationDetailView.swift
@@ -9,7 +9,70 @@ import SwiftUI
 
 struct LocationDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading) {
+                Image("default-banner-asset")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom)
+                
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 15)
+                        .foregroundColor(.secondary)
+                    
+                    Text("15 Market St Ste 40")
+                        .font(.system(size: 12, weight: .thin, design: .serif))
+                        .foregroundColor(.secondary)
+                    
+                }
+                .padding(.leading)
+                
+            }
+            .padding(.bottom, 40)
+            
+            Text("It is Chipotle. Enough said.")
+            
+           
+            
+            HStack(spacing: 20) {
+                Button(action: {}, label: {
+                    Image(systemName: "location.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                        .foregroundColor(.brandPrimary)
+                })
+                
+                Button(action: {}, label: {
+                    Image(systemName: "network")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                        .foregroundColor(.brandPrimary)
+                })
+                
+                Button(action: {}, label: {
+                    Image(systemName: "phone.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                        .foregroundColor(.brandPrimary)
+                })
+                
+                Button(action: {}, label: {
+                    Image(systemName: "location.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                })
+            }
+            
+            Spacer()
+        }
+            
     }
 }
 
