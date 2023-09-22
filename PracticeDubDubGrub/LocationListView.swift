feca9b2ce9409ff285458425ca8153aed61305a6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-22 09:56:01 -0600

    --Progress: extracted an HStacl to then embed the new struct into a NavigationLink, not passing in a Unique name yet

--- PracticeDubDubGrub/LocationListView.swift
+++ PracticeDubDubGrub/LocationListView.swift
@@ -12,29 +12,8 @@ struct LocationListView: View {
         NavigationView {
             List {
                 ForEach(0..<10) { item in
-                    HStack {
-                        Image("default-square-asset")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 80)
-                            .clipShape(Circle())
-                            .padding(.vertical, 6)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Test Location")
-                                .font(.system(size: 32, weight: .thin, design: .serif))
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.75)
-                            
-                            HStack {
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                            }
-                        }
-                        .padding(.leading)
+                    NavigationLink(destination: LocationDetailView()) {
+                        LocationCell()
                     }
                 }
             }
@@ -65,5 +44,35 @@ struct AvatarView: View {
             .scaledToFit()
             .frame(height: size)
             .clipShape(Circle())
+    }
+}
+
+struct LocationCell: View {
+    
+    var body: some View {
+        HStack {
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 6)
+            
+            VStack(alignment: .leading) {
+                Text("Test Location")
+                    .font(.system(size: 32, weight: .thin, design: .serif))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                
+                HStack {
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                }
+            }
+            .padding(.leading)
+        }
     }
 }
