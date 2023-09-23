f6f6162a90c56604c19e68e60acaf18b7a4667a1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-23 01:28:48 -0600

    --Progress: Extracting and refactoring logic and reusable code from Views to simplify and clean project

---
+++ PracticeDubDubGrub/AvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarView.swift
+//  PracticeDubDubGrub
+//
+//  Created by Mitch Andrade on 9/23/23.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvatarView()
+}
---
+++ PracticeDubDubGrub/LocationCell.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCell.swift
+//  PracticeDubDubGrub
+//
+//  Created by Mitch Andrade on 9/23/23.
+//
+
+import SwiftUI
+
+struct LocationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationCell()
+}
